6dc07550a590c9843958c47b232f8c9f961f912a
manthri11 <manthri11@gmail.com>
2020-03-02 16:10:32 -0800

    Create execute..sh

---
+++ execute..sh
@@ -0,0 +1,3 @@
+sudo docker run -dit --name master ./jmeter-master /bin/bash
+sudo docker run -dit --name slave1 ./jmeter-slave /bin/bash
+sudo docker run -dit --name slave2 ./jmeter-slave /bin/bash
